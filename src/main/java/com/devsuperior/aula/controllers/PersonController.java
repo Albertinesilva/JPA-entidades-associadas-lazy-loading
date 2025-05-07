80c2865e2eb57f9ec2fe855e29e6deaed296e72d
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2025-05-07 16:32:20 -0300

    Salvando entidade associada para um, utilizando exemplo com objeto mapeado pela JPA e também de froma transiente

---
+++ src/main/java/com/devsuperior/aula/controllers/PersonController.java
@@ -0,0 +1,32 @@
+package com.devsuperior.aula.controllers;
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.devsuperior.aula.dto.PersonDepartmentDTO;
+import com.devsuperior.aula.services.PersonService;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RestController
+@RequestMapping(value = "/people")
+public class PersonController {
+
+  @Autowired
+  private PersonService personService;
+
+  @PostMapping
+  public ResponseEntity<PersonDepartmentDTO> insert(@RequestBody PersonDepartmentDTO dto) {
+    PersonDepartmentDTO person = personService.insert(dto);
+    URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+        .path("/{id}")
+        .buildAndExpand(person.getId())
+        .toUri();
+    return ResponseEntity.created(uri).body(person);
+  }
+}
